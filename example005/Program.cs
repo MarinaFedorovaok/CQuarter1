bde6a7fb837ee1734b8055821e5d62547f758bcf
Marina F <fedma001@mail.ru>
2021-10-24 22:15:42 +0300

    add username

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+   Console.WriteLine("Hi, this is Masha");
+   }
+else
+{
+   Console.Write("Hi, ");
+   Console.WriteLine(username);
+}
