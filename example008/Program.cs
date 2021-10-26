ee054d33625f92e803ab1e63e72cebeb87b8c059
Marina F <fedma001@mail.ru>
2021-10-26 21:05:41 +0300

    example 008 done - find max using function

---
+++ example008/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+
+int a1 = 3124;
+int b1= 4114;
+int c1 = 3124;
+int a2= 14;
+int b2= 414;
+int c2 = 324;
+int a3= 411;
+int b3= 414;
+int c3 = 324;
+
+int max= Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
+
+Console.WriteLine(max);
