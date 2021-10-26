5127a49e5fcaa51ce21fed94fc12e67f4e8cf75b
Marina F <fedma001@mail.ru>
2021-10-26 22:21:04 +0300

    array add example 010

---
+++ example10/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 13, 143, 14734, 53, 4212};
+int n = array.Length;
+int find = 143;
+int index= 0;
+while (index < n)
+{
+    if (array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+        index++;
+}
