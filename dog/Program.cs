c8170d71149f7df9ac0028d26d5e0be1dacf23d8
Marina F <fedma001@mail.ru>
2021-10-26 13:12:41 +0300

    first made and mistake

--- dog/Program.cs
+++ dog/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 2;
+int secondFrieidSpeed = 1;
+int dogSpeed = 3;
+int Friend = 2;
+int time = 0;
+while distance > 0
+    {
+        if Friend = 1
+            {
+                time= distance/(firstFriendSpeed+dogSpeed);
+                Friend=2;
+            }
+            else
+            {
+                time= distance/(secondFrieidSpeed+dogSpeed);
+                Friend=1;
+            }
+        distance = distance-(firstFriendSpeed+secondFrieidSpeed)*time;
+        count++;
+    }
+    Console.Write(count);
