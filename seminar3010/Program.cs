9a0d12c32e33470276e1b4cf67bae4459764a875
Marina F <fedma001@mail.ru>
2021-10-30 19:40:13 +0300

    compares with zero but works

---
+++ seminar3010/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// int i = 0;
+// while (i < 10)
+
+// {
+
+//     i += 2;
+//     Console.WriteLine (i);
+
+// // }
+
+// int [] arr = new int[] {1, 2, 3, 8, 12 }
+// int k = GetNumber();
+
+
+//**Задание**
+
+// Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue].
+// Создать на его основе масив B, отбрасывая те, которые нарушают порядок
+// возрастания
+// элементы, больше 8
+// знакочередования
+
+int [] A = new int [10];
+int index = 0;
+while (index < 10)
+{
+    A[index] = new Random().Next(10, 100);
+    index++;
+}
+int [] B = new int [10];
+
+index = 0;
+while (index <10)
+{
+    int val = A[index];
+    Console.WriteLine(A[index]);
+    index++;
+}
+Console.WriteLine("*");
+index = 0;
+int indexb = 0;
+ while (index < 10)
+{
+    if (A[index] < A[index+1])
+    {
+        B [index]=A [index];
+    }
+        Console.WriteLine(B[index]);
+        index++;
+    }
+
+
+// int find = 143;
+// int index= 0;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//         {
+//             Console.WriteLine(index);
+//             break;
+//         }
+//         index++;
+// }
