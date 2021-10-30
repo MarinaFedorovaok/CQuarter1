310bc7b82ce155931c558dd8981e432e8ea039b5
Marina F <fedma001@mail.ru>
2021-10-30 23:13:15 +0300

    added conditions files

---
+++ condition2/Program.cs
@@ -0,0 +1,40 @@
+﻿//**Задание**
+
+// Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue].
+// Создать на его основе масив B, отбрасывая  
+// элементы, больше 8
+
+
+int [] A = new int [10];
+int index = 0;
+while (index < 10)
+{
+    A[index] = new Random().Next(0, 10);
+    Console.WriteLine(A[index]);
+    index++;
+}
+int [] B = new int [10];
+
+// index = 0;
+// while (index <10)
+// {
+//     int val = A[index];
+//     Console.WriteLine(A[index]);
+//     index++;
+// }
+Console.WriteLine("*");
+// index = 0;
+// B [index]=A [index];
+// Console.WriteLine(B[index]);
+index = 0;
+ while (index < 10)
+{
+    if (A[index] < 8)
+    {
+        B[index]=A[index];
+        Console.WriteLine(B[index]);
+        index=index++;
+
+    }
+       index++;
+ }
---
+++ condition3/Program.cs
@@ -0,0 +1,31 @@
+﻿//**Задание**
+
+// Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue].
+// Создать на его основе масив B, убрав те элементы, которые нарушают порядок
+// знакочередования
+
+
+int [] A = new int [10];
+int index = 0;
+while (index < 10)
+{
+    A[index] = new Random().Next(-5, 5);
+    Console.WriteLine(A[index]);
+    index++;
+}
+int [] B = new int [10];
+
+Console.WriteLine("*");
+
+index = 0;
+ while (index < 10)
+{
+    if (A[index] < 0)
+    {
+        B[index]=A[index];
+        Console.WriteLine(B[index]);
+        index=index++;
+
+    }
+       index++;
+ }
