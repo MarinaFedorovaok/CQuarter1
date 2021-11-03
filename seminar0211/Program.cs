0b24dc22773a0514168fd4e11ba8f1d8cfd6b82e
Marina F <fedma001@mail.ru>
2021-11-03 11:36:19 +0300

    array sorted from max to min works

--- seminar0211/Program.cs
+++ seminar0211/Program.cs
@@ -107,25 +107,21 @@ void SelectoinSort(int[] array)
 
     for (int i=0; i<array.Length - 1 ; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
 
          for (int j=i+1; j<array.Length; j++)
 
          {
-            if(array[j]< array[minPosition]) minPosition = j;
+            if(array[j]> array[maxPosition]) maxPosition = j;
 
          }
 
         //
         int temp = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temp;
-
-
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
 
     }
-
-
 
 }
 
