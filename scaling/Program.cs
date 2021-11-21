11283006667b74d8a77ddd7cd124b951381ff63e
Marina F <fedma001@mail.ru>
2021-11-21 09:42:20 +0300

    46 works first line method

---
+++ scaling/Program.cs
@@ -0,0 +1,42 @@
+﻿// 46. Написать программу масштабирования фигуры
+// Исходные данные:  фигура нарисована значками и надо нарисовать в н раз большую
+
+int[,] pic = new int[,]
+{
+    {1, 0, 1, 1, 0},
+    {0, 1, 0, 1, 0},
+    {0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 1},
+    {0, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0},
+};
+
+void PrintImage1(int[,] pic) //Cоздаем метод выведения фигуры в первом массиве
+{
+    for (int i = 0; i < pic.GetLength(1); i++)
+    {
+        for (int j = 0; j < pic.GetLongLength(1); j++)
+        {
+            //Console.Write($"{matr[i, j]} ");
+            if (pic[i, j] == 0) Console.Write(" ");
+            else Console.Write("@");
+
+        }
+        Console.WriteLine();
+    }
+}
+PrintImage1(pic); // выводим первую фигуру
+Console.WriteLine("-----------------");
+
+void PrintScalingRow(int[,] pic, int NumROW, int n) //Cоздаем метод выведения отмасштабированной строки
+{
+    for (int i = 0; i < pic.GetLength(1); i++) //проверяем выход за строку
+    {
+        for (int m = 0; m<n; m++) //задаем счетчик масштабирования
+        {
+            if (pic[NumROW, i] == 0) Console.Write(" ");
+            else Console.Write("@");
+        }
+    }
+}
+PrintScalingRow(pic, 0, 2); // масштабируем первую строку
