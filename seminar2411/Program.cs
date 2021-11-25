c70f2d44a6cdb2b3537c6e740e624027f48ea71f
Marina F <fedma001@mail.ru>
2021-11-25 12:27:59 +0300

    works with methods

--- seminar2411/Program.cs
+++ seminar2411/Program.cs
@@ -1,58 +1,57 @@
-﻿// GameInit() - метод, который задает все исходные значения в вашей игре.
+﻿// * GameInit() - метод, который задает все исходные значения в вашей игре.
 // * MakeMove() - ходы, задаются этим методом.
-Console.WriteLine("Привет! Введи интервал чисел и количество попыток, я загадаю число из этого интервала, а тебе нужно будет его угадать");
-Console.WriteLine("Введи начало интервала: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введи конец интервала: ");
-int max = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введи количество попыток: ");
-int countMax = Convert.ToInt32(Console.ReadLine());
-
-int SecretNumber = new Random().Next(min, max);
-int AscMethod()
+// * RequestNumber() - запрос числа у игрока, обработка нештатных ситуаций.
+// * CheckNumber(int PlayesrNumber, int SecretNumber)
+// * GameOver(bool winner) - 
+// int requestNumber() - метод запросит у игрока число, сделает все необходимые преобразования и вернет нам это число.
+// int createNumber(int leftBound, int rightBound) - загадает число в указанном диапазоне и вернет его нам.
+// bool MakeMove(int SecretNumber, int CountOfAttempts) - метод, в котором запрограммирован 1 игровой ход, в результате метод возвращает либо true, либо false. true если пользователь угадал число и игру надо остановить, false, если пользователь не угадал.
+int GameInit() //метод получает исходные данные и хранит секретное число
+{
+    int[] inputData = new int[2];
+    Console.WriteLine("Привет! Введи интервал чисел и количество попыток, я загадаю число из этого интервала, а тебе нужно будет его угадать");
+    Console.WriteLine("Введи начало интервала: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введи конец интервала: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int res = new Random().Next(min, max);  //запоминает секретное число
+    return res;
+}
+int AscMethod() //метод хранит число игрока
 {
     Console.WriteLine("Bведи свое предположение: ");
     int PlayesrNumber = Convert.ToInt32(Console.ReadLine());
     return PlayesrNumber;
 }
-Console.WriteLine(SecretNumber);
-void MakeMove()
+int MakeMove(int count) // метод делает ход и проверяет, хранит ответ/ исходные данные - количество попыток
 {
-    while (countMax >= 0)
+    int SecretNumber = GameInit();
+    while (count >= 0)
     {
         int PlayesrNumber = AscMethod();
         if (PlayesrNumber > SecretNumber)
         {
             Console.WriteLine("Загаданное число меньше введенного");
-            countMax = countMax - 1;
+            count--;
         }
         else
-
         {
             if (PlayesrNumber < SecretNumber)
             {
 
                 Console.WriteLine("Загаданное число больше введенного");
-                countMax--;
+                count--;
             }
             else
             {
                 Console.WriteLine("Победа!");
-                return;
+                return SecretNumber;
             }
         }
     }
     Console.WriteLine("В этот раз не получилось");
+    return SecretNumber;
 }
-// void GameOver(int )
-// {
-//     if (AscMethod() == SecretNumber) Console.WriteLine("Победа!");
-//     else
-//     {
-//         Console.WriteLine("В этот раз не получилось");
-//     }
-// }
-MakeMove();
-// * RequestNumber() - запрос числа у игрока, обработка нештатных ситуаций.
-// * CheckNumber(int PlayesrNumber, int SecretNumber)
-// * GameOver(bool winner) - 
+
+Console.WriteLine(MakeMove(3));
+
