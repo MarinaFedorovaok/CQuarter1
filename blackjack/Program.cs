f854e9ad50e6ca060baa6d8eca020dadfc44c7d4
Marina F <fedma001@mail.ru>
2021-11-28 20:51:49 +0300

    showing winner with ignoring draw

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -1,91 +1,13 @@
 ﻿// Игра "Blackjack"
-// Описание правил игры можно почитать на вот тут
 
-// Значения очков каждой карты: от двойки до десятки — от 2 до 10 соответственно, 
-//у туза — 1 или 11 (11 пока общая сумма не больше 21, далее 1), 
-// у т. н. картинок (король, дама, валет) — 10.
-
-// Идея решения
-// В игре участвуют несколько игроков, для простоты можно взять двух игроков. 
-// Каждый игрок по очереди отвечат на вопрос - "нужна ли Вам еще одна карта?".
-//  Если ответ утвердительный, то игроку выдается следующая карта из колоды. 
-//  Если игрок ответил отрицательно, то у этого игрока больше не спрашиваем. 
-//  Когда все игроки отказались от новых карт, то начинается подсчет очков.
-//   В результате определяется победитель.
-
-// Для начала надо определиться как мы будем работать с картами. Напомню, в задаче нельзя пользоваться ничем кроме массивов.
-//  Также важно понять как производить итоговый подсчет очков у каждого игрока, 
-//  то есть надо как-то узнать сколько очков дают за карты, которые находятся на руках у каждого из игроков.
-
-// Сама игра проходит так:
-
-// Создаем колоду карт.
-//int [] CreateCards(int n)// -метод создает колоду карт в зависмости от количества карт в колоде. 
-//        Итог работы метода - массив
-// Значения очков каждой карты: от двойки до десятки — от 2 до 10 соответственно, 
-//у туза — 1 или 11 (11 пока общая сумма не больше 21, далее 1), 
-// у т. н. картинок (король, дама, валет) — 10.
-
-//В колоде по 4 карты от 2 до 9, 11, и 7 карт 10. Есть счетчик, берем карты по порядку. 
-// Идем по массиву
-//****** Владимир
-
-// Для игры нам необходима перемешанная колода, поэтому надо в случайном порядке перетасовать карты (или сделать какой-то механизм, чтобы менять порядок карт)
-//int Turn (int playerNum, int [] Arr) // принимает номер игрока, массив с картами. Выясняет, хочет ли игрок делать ход
-// присваивает значение переменным bool wantsCards0 = true;
-// bool wantsCards1 = true;
-// Возвращает количество очков игрока
-// Фаза набора карт игроками. Каждый игрок берет по одной карте, до тех пока не скажет что ему больше карты не нужны. Карты выдаются по одной из перемешанной колоды.
-// Фаза подсчета очков. Нам необходимо узнать сколько очков у каждого игрока.
-// Определение победителя в этом раунде.
-// i++
-//***Анастасия***
-
-
-// Метод определения веса туза (вызывется если карта  = 11)
-// ***Евгений
-
-//int [] score () // Выдает массив с картами игрока 
-
-// **Марина
-//bool oneWantsCards (bool player1, bool player2) // метод определяет, надо ли делать следующий ход
-// y -yes n - no
-
-//int winner (int score1, int score2) //метод определяет победителя в раунде
-// Можно продолжить играть и перейти к следующему раунду (шаг 3).
-//**************************************
-//Материалы для работы:
-// Как задать динамический массив:
-
-//List<int> col= new();
-
-// Чтобы добавить элемент пишем
-//col.Add(123);
-
-//Чтобы удалить col.Remove(123);
-
-//Узнать количество элементов : col.Count
-// int[] cards = CreateCards(52);
-// int score0  
-// int score1 ;
-// int i = 0;
-// bool wantsCards0 = true;
-// bool wantsCards1 = true;
-// while oneWantsCards(wantsCards0, wantsCards1)
-// {
-//     score0 = Turn(0);
-//     score1 = Turn(1);
-// }
-// winner(score0, score1);
-
-int[] mixCards(int[] cards) // метод сортировки колоды
+int[] mixCards(int[] cards) // альтернативный метод сортировки колоды
 {
     int[] randomArr = new int[cards.Length];  //создали вспомогательный массив
     for (int i = 0; i < randomArr.Length; i++)
     {
         randomArr[i] = new Random().Next(0, 100); // заполняем вспомогательный массив случайными числами
     }
-    for (int i = 0; i < randomArr.Length; i++) //делаем пузырьковую сортировку вспом массива и параллельно мешаем наш массив
+    for (int i = 1; i < randomArr.Length; i++) //делаем пузырьковую сортировку вспом массива и параллельно мешаем наш массив
     {
         int min = randomArr[0];
         int buff = min;
@@ -112,4 +34,112 @@ string PrintArray(int[] array) //Создаем метод выведения м
     return res;
 }
 int [] Arr = {1, 4, 6, 8, 14, 1, 4, 6, 8, 14,1, 4, 6, 8, 14};
-Console.Write(PrintArray(mixCards(Arr)));
+// Console.Write(PrintArray(mixCards(Arr)));
+
+
+
+// using System;
+
+namespace Game
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            int[] deckOfCards =
+            {2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6,
+            7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11,
+            11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14};    // колода карт
+            int[] mixedCards = Mixing(deckOfCards);     // Сортировка колоды
+            byte PlayerNum = 1;
+            int iCard = 0; // Индекс карты из массива колоды
+            Game(PlayerNum, mixedCards, iCard);     // Запуск игры 
+        }
+
+        public static int[] Mixing(int[] array)
+        {        // Метод тасует колоду
+            int j, temporary;
+            for (int i = 0; i < array.Length; i++)
+            {
+                temporary = array[i];
+                j = new Random().Next(i, array.Length);
+                array[i] = array[j];
+                array[j] = temporary;
+            }
+            return array;
+        }
+
+        public static string NameCard(int a)
+        {      // Метод определяет название карты
+            string[] nameCards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Туз", "Валет", "Дама", "Король" };
+            return nameCards[a - 2];
+        }
+
+        public static bool Answer(string? str) => str == "y";        // Метод возвращает TRUE если игрок согласен
+
+        public static void Print(string str) { Console.WriteLine(str); }       // Достало писать каждый раз cw и удалять System
+
+        public static (int sum, int count) Turn(int Player, int[] cards, int iCard)
+        {      // Метод подсчета очков (возвращает кортеж (сумма очков, индекс карты))
+            var result = (sum: 0, count: iCard);
+            int sumTus = 0, Tus = 0, officer = 0;
+            int sumICard = iCard;
+            if (cards[iCard] > 11) officer = cards[iCard] - 10;
+            if (cards[iCard + 1] > 11) officer += cards[iCard + 1] - 10;
+            result.sum = cards[iCard] + cards[iCard + 1] - officer;
+            officer = 0;
+            if (cards[iCard] == 11) sumTus += 10;
+            if (cards[iCard + 1] == 11) sumTus += 10;
+            if (result.sum > 21) result.sum -= sumTus;
+
+            Print($"Игрок {Player}, тебе выпали карты {NameCard(cards[iCard])} и {NameCard(cards[iCard + 1])}.\nИх сумма {result.sum}. Желаешь взять ещё карту? y/n");
+            iCard += 2; // 3я карта в массиве от старта игрока
+            while (Answer(Console.ReadLine()))
+            {
+                if (cards[iCard] > 11) officer = cards[iCard] - 10;
+                result.sum += cards[iCard] - officer;
+                officer = 0;
+                if (cards[iCard] == 11) Tus += 10;
+                if (result.sum > 21 && sumTus == 10) { result.sum -= sumTus; sumTus = 0; }
+                if (result.sum > 21) { result.sum -= Tus; Tus = 0; }
+                Print($"Вот тебе еще карта: {NameCard(cards[iCard])}. Сумма карт {result.sum}. Ещё? y/n");
+                iCard++;
+            }
+            result.count = iCard;
+            Print($"Sum cards Player {Player}: {result.sum}"); //// строка для проверки
+            return result;
+        }
+        public static void PrintWinner(int[] score) //метод печатает победителя, принимает массив с очками игроков
+        {
+            int max = 0;
+            int winner = -1;
+            for (int i = 0; i < score.Length; i++)
+            {
+                if (score[i] > max && score[i] <= 21) max = score[i];
+                winner = i + 1;
+            }
+            if (max == 0) Console.WriteLine("Ничья!");
+            else Console.WriteLine($"Победил игрок {winner}, набрав {max} очков");
+        }
+        public static void Game(int PlayerNum, int[] mixedCards, int iCard)
+        {      // Метод игры для n-x игроков 
+
+            (int sum, int count) countCard = (0, 0);
+            int n = 3;
+            int[] score = new int[n];
+            while (PlayerNum <= n)
+            {
+                countCard = Turn(PlayerNum, mixedCards, iCard);
+                iCard = countCard.count;
+                Print("Использовано карт в игре: " + iCard.ToString());
+                score[PlayerNum-1] = countCard.sum;
+                PlayerNum++;
+            }
+            PrintWinner(score);
+        }
+
+    }
+
+}
