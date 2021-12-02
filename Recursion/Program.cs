f4d8f7593aa20805f536f287167d939c726fdf05
Marina F <fedma001@mail.ru>
2021-12-02 21:22:21 +0300

    68 works with different order

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -19,11 +19,11 @@
 // 68. Показать натуральные числа от M до N, N и M заданы
 string PrintNM(int m, int n)
 {
-    if (m>n) return "";
+    if (m > n) return $"{PrintNM(n, m)}";
+    if (m == n) return $"{m}";
     else
     {
         return $"{PrintNM(m, n - 1)} {n}";
     }
-
 }
-Console.WriteLine(PrintNM(3, 25));
+Console.WriteLine(PrintNM(33, 25));
