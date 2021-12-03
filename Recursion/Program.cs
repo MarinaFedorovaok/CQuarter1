dc64cf1d3d8840d9ad105babd00a9e8f10d9ffc5
Marina F <fedma001@mail.ru>
2021-12-03 10:56:06 +0300

    69 works

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Рекурсия
 // 66. Показать натуральные числа от 1 до N, N задано
-// string PrintN(int n)
-// {
-//     if (n < 2) return "1";
-//     else
-//     {
-//         return $"{PrintN(n - 1)} {n}";
-//     }
-// }
+string PrintN(int n)
+{
+    if (n < 2) return "1";
+    else
+    {
+        return $"{PrintN(n - 1)} {n}";
+    }
+}
 //Console.WriteLine (PrintN (5));
 
 // 67. Показать натуральные числа от N до 1, N задано
-// string PrintN2(int n)
-// {
-//     return n < 2 ? "1" : $"{n} {PrintN2(n - 1)}";
-// }
+string PrintN2(int n)
+{
+    return n < 2 ? "1" : $"{n} {PrintN2(n - 1)}";
+}
 //Console.WriteLine(PrintN2(5));
 // 68. Показать натуральные числа от M до N, N и M заданы
 string PrintNM(int m, int n)
@@ -26,4 +26,15 @@ string PrintNM(int m, int n)
         return $"{PrintNM(m, n - 1)} {n}";
     }
 }
-Console.WriteLine(PrintNM(33, 25));
+//Console.WriteLine(PrintNM(33, 25));
+
+//69. Найти сумму элементов от M до N, N и M заданы
+int Summ (int m, int n)
+{
+    if (n==m) return n;
+    else
+    {
+        return n+Summ (m, n-1);
+    }
+}
+Console.WriteLine(Summ(2,6));
